21550548f1b3dade0429274df11785741212a17a
amtoaer <amtoaer@gmail.com>
2021-05-28 14:11:40 +0800

    refactor: 将filesystem单独拆分头文件，更新cmakelist

--- src/header/header.h
+++ src/header/header.h
@@ -16,7 +16,7 @@ using namespace std;
 #define DISKNUM 512      //磁盘总数
 #define SFDNUM   128       //目录总数
 
-extern int UserN=0;   //当前已有用户
+extern int UserN = 0;   //当前已有用户
 
 struct User {      //用户
     int id;     //用户id号
@@ -24,8 +24,9 @@ struct User {      //用户
     string userpwd;
     int usergrp;//用户组
     bool isManager; //管理员
-    User(){}
-    User(string name,string pwd,int grp,bool isM){
+    User() {}
+
+    User(string name, string pwd, int grp, bool isM) {
         id = UserN++;
         username = name;
         userpwd = pwd;
@@ -34,21 +35,21 @@ struct User {      //用户
     }
 };
 
-struct DiskBlock {	  //磁盘块
-    char content[BLOCKSIZE];	//文件内容
-    int n;		//已占空间
+struct DiskBlock {      //磁盘块
+    char content[BLOCKSIZE];    //文件内容
+    int n;        //已占空间
 };
 
-struct RWCT{    //读写控制块
+struct RWCT {    //读写控制块
     int user_id;    //属主id
     vector<int> read_group; //只读组 存放用户id
     vector<int> raw_group;  //只写 存放用户id
     vector<int> invisible_group;    //不可见组 存放用户id
 };
 
-struct Inode {			//内存i结点
+struct Inode {            //内存i结点
     RWCT rwct;          //读写控制表
-    int i_size;		    //文件大小
+    int i_size;            //文件大小
     int diskblock_num;  //文件占用磁盘块个数
     int diskblock_id;   //第一块磁盘块id
     int i_count;        //文件的引用数
@@ -58,9 +59,9 @@ struct Inode {			//内存i结点
 
 };
 
-struct Dinode {			//i结点
+struct Dinode {            //i结点
     RWCT rwct;          //读写控制表
-    int i_size;		    //文件大小
+    int i_size;            //文件大小
     int diskblock_num;  //文件占用磁盘块个数
     int diskblock_id;   //第一块磁盘块id
 
@@ -70,12 +71,12 @@ struct Dinode {			//i结点
 
 };
 
-struct SuperBlock {		 //超级块结构
+struct SuperBlock {         //超级块结构
     int freeinode_num;      //空闲i结点总数
     vector<int> freeInode; //空闲i结点id数组
 
     int freedisk_num;        //空闲磁盘块块数
-    int freeDiskStack[NICFREE+1]; //成组链接空闲磁盘块栈
+    int freeDiskStack[NICFREE + 1]; //成组链接空闲磁盘块栈
 };
 
 struct SFD_ITEM {
@@ -84,34 +85,26 @@ struct SFD_ITEM {
     int id;         //目录：下一级目录索引  文件：磁盘i结点
 };
 
-struct MFD_ITEM{
-    int  userid;   //用户名
+struct MFD_ITEM {
+    int userid;   //用户名
     int dir_id; //目录id
 };
 
-struct SFD{
+struct SFD {
     int num;
     vector<SFD_ITEM> dir;
-    SFD(){
-        num=0;
+
+    SFD() {
+        num = 0;
     }
 };
 
-struct BFD{
+struct BFD {
     int ffd_num;        //空闲目录数
     vector<int> FFD;    //空闲目录id
 
     int mfd_num;          //用户总数
     vector<MFD_ITEM> MFD; //用户目录id
-
-};
-
-struct FileSystem {			 //文件系统结构
-
-    SuperBlock superBlock;   //超级块
-    Dinode diNode[DINODENUM];        //i结点
-    DiskBlock diskBlock[DISKNUM];//磁盘块
-    SFD sfd[SFDNUM];                   //目录块
 
 };
 
@@ -121,6 +114,6 @@ extern bool diskBitmap[DISKNUM];    //磁盘块结点位示图
 extern bool sfdBitmap[SFDNUM];      //目录位示图
 extern int sfd_id;      //当前目录id
 extern User Users[USERNUM];  //全部用户
-extern  User cur_user;  //当前用户
+extern User cur_user;  //当前用户
 
 #endif //OS_FILE_MANAGER_HEADER_H
---
+++ src/header/system.h
@@ -0,0 +1,18 @@
+//
+// Created by amtoaer on 2021/5/28.
+//
+
+#ifndef OS_FILE_MANAGER_SYSTEM_H
+#define OS_FILE_MANAGER_SYSTEM_H
+
+class FileSystem {             //文件系统结构
+
+    SuperBlock superBlock;   //超级块
+    Dinode diNode[DINODENUM];        //i结点
+    DiskBlock diskBlock[DISKNUM];//磁盘块
+    SFD sfd[SFDNUM];                   //目录块
+    
+    FileSystem() {}
+};
+
+#endif //OS_FILE_MANAGER_SYSTEM_H
