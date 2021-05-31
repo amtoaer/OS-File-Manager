7ec353c2ac6e841829969cc978b6c2f69db36e91
me1l1n <495851184@qq.com>
2021-05-31 16:02:08 +0800

    feat: 增加用户登陆、注册、注销功能

--- src/header/user.h
+++ src/header/user.h
@@ -12,17 +12,45 @@ using namespace std;
 
 
 class User {      //用户
-    int id;         //用户id号
+    int id = -1;         //用户id号
     string username; //用户名
     string userpwd;   //密码
 public:
     User() {};
 
-    User(int id, string name, string pwd);
+    User(int id) {
+        this->id = id;
+    }
+
+    User(int id, string name, string pwd) {
+        this->id = id;
+        this->username = name;
+        this->userpwd = pwd;
+    }
 
     //获取用户id
     int getId() {
         return id;
+    }
+
+    void setId(int id) {
+        this->id = id;
+    }
+
+    void setUsername(string username) {
+        this->username = username;
+    }
+
+    bool isUsername(string username) {
+        return this->username == username;
+    }
+
+    void setUserpwd(string userpwd) {
+        this->userpwd = userpwd;
+    }
+
+    bool isUserpwd(string userpwd) {
+        return this->userpwd == userpwd;
     }
 };
 
@@ -30,8 +58,9 @@ class UserManage {
     User users[USERNUM];  //全部用户
     int user_num = 0;   //当前已有用户
 public:
-    bool addUser();     //添加用户
-    bool deleteUser();  //注销用户
+    bool addUser(string username, string userpwd);     //添加用户
+    bool deleteUser(string username, string userpwd);  //注销用户
+    User login(string username, string userpwd);       //登陆用户
     vector<int> getUserId();    //获取全部用户的id号
 };
 
