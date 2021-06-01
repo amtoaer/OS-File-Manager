37daec4a1259e05035d664945301c1a6d390e057
amtoaer <amtoaer@gmail.com>
2021-06-01 15:24:06 +0800

    feat: 文件夹支持删除、查找条目

--- src/source/dir.cpp
+++ src/source/dir.cpp
@@ -9,6 +9,30 @@ bool SFD::addItem(SFD_ITEM sfd_item) {
     return true;
 }
 
+int SFD::hasNext(string name) {
+    for (int i = 0; i < num; i++) {
+        if (dir[i].name == name) {
+            return dir[i].type;
+        }
+    }
+    return 0;
+}
+
+SFD_ITEM SFD::removeNext(string name) {
+    for (int i = 0; i < num; i++) {
+        if (dir[i].name == name) {
+            auto result = dir[i];
+            for (int j = i + 1; j < num; j++) {
+                dir[j - 1] = dir[j];
+            }
+            num--;
+            return result;
+        }
+    }
+    // 需要确保文件/文件夹已存在才可调用该方法，否则会返回新的SFD_ITEM
+    return SFD_ITEM();
+}
+
 int SFD::getNextDir(string dirname) {
     int sfd_id = -1;
     SFD_ITEM item;
